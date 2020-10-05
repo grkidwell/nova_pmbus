5a97944b66ebcef23d18694a611d49f27b8fc1f9
grkidwell <gkidwell@mac.com>
2020-10-05 11:03:17 -0500

    add config_2_ram.ipyb, project txt file, and other tweaks

--- lib/dongle_cmds.py
+++ lib/dongle_cmds.py
@@ -24,16 +24,16 @@ def list_and_str_2int(x):
 def read_smb_cmd(cmd_code, data_length,dev_addr):
     cmd_code = list_and_str_2int(cmd_code)
     dummy_read(dev_addr)
-    time.sleep(.25)
+    time.sleep(.005)
     
     try:
         result = dongle.read(reg_addr=cmd_code, size=data_length, slave_addr=dev_addr)
-        time.sleep(0.25)
+        time.sleep(0.005)
     except:
-        time.sleep(0.25)
+        time.sleep(0.005)
         print('error')
         result = dongle.read(reg_addr=cmd_code, size=data_length, slave_addr=dev_addr)
-        time.sleep(0.25)
+        time.sleep(0.005)
     result = dongle.read(reg_addr=cmd_code, size=data_length, slave_addr=dev_addr)
     return result
     
@@ -42,9 +42,9 @@ def write_smb_cmd(cmd_code, data_length, data, dev_addr):
     data = [int(b, 16) for b in data]
     dongle.write(cmd_code, data_length, data, slave_addr=dev_addr)
     
-def write_dma_cmd(reg,data,data_length,dev_addr):
+def write_dma_cmd(reg,data_length,data,dev_addr):    #swapped length and data
     write_smb_cmd(0xc7,2,reg,dev_addr)
-    time.sleep(0.25)
+    time.sleep(0.005)
     write_smb_cmd(0xc5,4,data,dev_addr)
         
 def read_dma_cmd(reg, data_length, dev_addr):
@@ -56,9 +56,9 @@ def read_dma_cmd(reg, data_length, dev_addr):
 #    except:
     data_length = 4
     write_smb_cmd(0xc7,2,reg,dev_addr)
-    time.sleep(0.25)
+    time.sleep(0.005)
     read_smb_cmd(0xc5,data_length,dev_addr)
-    time.sleep(0.25)
+    time.sleep(0.005)
     return read_smb_cmd(0xc5,data_length,dev_addr)
 
 def bytearray2mv(bytearray):
